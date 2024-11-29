5d10b959af631d62b63aac5bdaf5cc5b848cb751
Ilias Rais <73229695+artfull-boy@users.noreply.github.com>
2024-11-29 07:18:35 +0000

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,11 @@
 use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/projects', [ProjectController::class, 'index']);
-
-Route::get('/projects/{project}', [ProjectController::class, 'show']);
-
-Route::post("/projects", [ProjectController::class, 'store']);
-
-Route::put("/projects/{project}",[ProjectController::class,'update']);
-
-Route::delete("/projects/{project}",[ProjectController::class,'destroy']);
+Route::middleware('verify.token')->group(function () {
+    Route::get('/projects', [ProjectController::class, 'index']);
+    Route::get('/projects/{project}', [ProjectController::class, 'show']);
+    Route::post("/projects", [ProjectController::class, 'store']);
+    Route::put("/projects/{project}", [ProjectController::class, 'update']);
+    Route::delete("/projects/{project}", [ProjectController::class, 'destroy']);
+});
 
